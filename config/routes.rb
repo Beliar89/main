613f4ed29e18a83ee19a5820135219cb9440e9ec
Marc89 <drakeunch@hotmail.com>
2017-05-26 12:54:32 +0200

    afegir taula top pelis

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
+  
+
+  devise_for :users
+
+  get 'statics/index'
+
+  get 'statics/about'
+
 	resources :peliculas
 	resources :comentarios
+  
   get 'comentarios/index'
-
   get 'peliculas/index'
+  get '/about' => 'static#about', :as => :about
+
+  root :to => "statics#index"
+   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
