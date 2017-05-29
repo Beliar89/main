9339401b3ab9364a93adb03ae90a19506a22a8ec
Marc89 <drakeunch@hotmail.com>
2017-05-29 10:14:09 +0200

    vven

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :usuarios
+  get 'statics/index'
+
+  get 'statics/about'
+
 	resources :peliculas
 	resources :comentarios
+  
   get 'comentarios/index'
-
   get 'peliculas/index'
+
+  root :to => "statics#index"
+
+  get '/about' => 'static#about', :as => :about
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
