82d28c2de974f82a7cf97fcc2ff987120b8b5019
Marc89 <drakeunch@hotmail.com>
2017-05-29 10:20:04 +0200

    9

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
   devise_for :usuarios
+  resources :peliculas
+  resources :comentarios
+  
   get 'statics/index'
 
   get 'statics/about'
-
-	resources :peliculas
-	resources :comentarios
-  
+  get '/about' => 'static#about', :as => :about
+	
   get 'comentarios/index'
   get 'peliculas/index'
 
   root :to => "statics#index"
 
-  get '/about' => 'static#about', :as => :about
+  
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
